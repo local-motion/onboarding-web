bbc67bc96c99e77a8cec7cc398674e2a392ed674
Errrrk <43416374+errrrk@users.noreply.github.com>
2018-10-15 08:59:05 +0200

    I18n integration with React

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+import {createBrowserHistory} from "history";
+import {Router, Route, Switch} from "react-router-dom";
+import {I18nextProvider} from "react-i18next";
+import i18n from "./i18n";
 
 import indexRoutes from "routes/index.jsx";
 
 import "assets/scss/material-kit-react.css?v=1.2.0";
 
-var hist = createBrowserHistory();
+let hist = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} key={key} component={prop.component} />;
-      })}
-    </Switch>
-  </Router>,
-  document.getElementById("root")
+    <I18nextProvider i18n={i18n}>
+        <Router history={hist}>
+            <Switch>
+                {indexRoutes.map((prop, key) => {
+                    return <Route path={prop.path} key={key} component={prop.component}/>;
+                })}
+            </Switch>
+        </Router>
+    </I18nextProvider>,
+    document.getElementById("root")
 );
---
+++ src/views/Onboarding/Onboarding.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// react components for routing our app without refresh
+// import {Link} from "react-router-dom";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+// @material-ui/icons
+// core components
+import Header from "components/Header/Header.jsx";
+import Footer from "components/Footer/Footer.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Parallax from "components/Parallax/Parallax.jsx";
+// sections for this page
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
+import componentsStyle from "assets/jss/material-kit-react/views/components.jsx";
+import PlaygroundMap from "./Sections/PlaygroundMap";
+
+import { withNamespaces } from "react-i18next";
+
+class Onboarding extends React.Component {
+    render() {
+        const {classes, ...rest} = this.props;
+        const { t, i18n } = this.props;
+        return (
+            <div>
+                <Header
+                    brand={t('header.brand')}
+                    rightLinks={<HeaderLinks/>}
+                    fixed
+                    color="transparent"
+                    changeColorOnScroll={{
+                        height: 400,
+                        color: "white"
+                    }}
+                    {...rest}
+                />
+                <Parallax image={require("assets/img/bg2.jpg")}>
+                    <div className={classes.container}>
+                        <GridContainer>
+                            <GridItem>
+                                <div className={classes.brand}>
+                                    <h1 className={classes.title}>Linnaeushof rookvrij!</h1>
+                                    <h3 className={classes.subtitle}>
+                                        Je hebt aangegeven dat jij Linnaeushof rookvrij wilt helpen maken
+                                    </h3>
+                                    <h3>
+                                        Help ons!
+                                    </h3>
+                                </div>
+                            </GridItem>
+                        </GridContainer>
+                    </div>
+                </Parallax>
+
+                <div className={classNames(classes.main, classes.mainRaised)}>
+                    <PlaygroundMap/>
+                </div>
+                <Footer/>
+            </div>
+        );
+    }
+}
+
+export default withStyles(componentsStyle)(withNamespaces("translations")(Onboarding));
