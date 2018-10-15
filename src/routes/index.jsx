8db58c2bd42abd5871a632321d7e0cc34708dd45
Errrrk <43416374+errrrk@users.noreply.github.com>
2018-10-15 08:59:05 +0200

    I18n integration with React

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,19 @@
 import Components from "views/Components/Components.jsx";
-import Workspace from "views/Workspace/Workspace.jsx";
 import LandingPage from "views/LandingPage/LandingPage.jsx";
 import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
 import LoginPage from "views/LoginPage/LoginPage.jsx";
+import Workspace from "views/Workspace/Workspace.jsx";
+import Onboarding from "views/Onboarding/Onboarding.jsx";
 
 var indexRoutes = [
     {path: "/profile-page", name: "ProfilePage", component: ProfilePage},
     {path: "/login-page", name: "LoginPage", component: LoginPage},
     {path: "/components-page", name: "Components", component: Components},
+    {path: "/landing-page", name: "LandingPage", component: LandingPage},
+
+
     {path: "/workspace", name: "Workspace", component: Workspace},
+    {path: "/onboarding", name: "Onboarding", component: Onboarding},
     {path: "/", name: "LandingPage", component: LandingPage}
 ];
 
