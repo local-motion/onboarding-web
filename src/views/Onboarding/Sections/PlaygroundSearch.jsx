b756b1e90d5ea8e716a9d13883f4d7905d1aaf3b
Errrrk <43416374+errrrk@users.noreply.github.com>
2018-10-22 11:12:43 +0200

    Switch to react-autocomplete
    Relates to local-motion/product#5

--- src/views/Onboarding/Sections/PlaygroundSearch.jsx
+++ src/views/Onboarding/Sections/PlaygroundSearch.jsx
@@ -3,17 +3,17 @@ import lunr from 'lunr'
 import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
 import {withNamespaces} from 'react-i18next';
-
-// @material-ui/icons
-import Search from "@material-ui/icons/Search";
-import image from "assets/img/bg.jpg";
-
 // core components
 import withStyles from "@material-ui/core/styles/withStyles";
 import Header from "components/Header/Header.jsx";
 import CustomInput from "components/CustomInput/CustomInput.jsx";
 import Button from "components/CustomButtons/Button.jsx";
 import navbarsStyle from "assets/jss/material-kit-react/views/componentsSections/navbarsStyle.jsx";
+import Autosuggest from 'react-autosuggest';
+
+// @material-ui/icons
+import Search from "@material-ui/icons/Search";
+// import image from "assets/img/bg.jpg";
 
 const GET_PLAYGROUNDS = gql`
     {
@@ -46,10 +46,23 @@ const withPlaygrounds = graphql(GET_PLAYGROUNDS, {
 );
 
 class SearchBar extends React.Component {
-    render() {
-        const {t, classes, playgrounds, onSelectPlayground} = this.props;
-        if (!playgrounds) return null;
+    constructor() {
+        super();
 
+        // Autosuggest is a controlled component.
+        // This means that you need to provide an input value
+        // and an onChange handler that updates this value (see below).
+        // Suggestions also need to be provided to the Autosuggest,
+        // and they are initially empty because the Autosuggest is closed.
+        this.state = {
+            value: '',
+            suggestions: []
+        };
+    }
+
+    // Teach Autosuggest how to calculate suggestions for any given input value.
+    getSuggestions = value => {
+        const playgrounds = this.props.playgrounds;
         const idx = lunr(function () {
             this.ref('id');
             this.field('name');
@@ -61,8 +74,71 @@ class SearchBar extends React.Component {
             }, this)
         });
 
-        let results = idx.search("Linn*");
+        function addPlaygroundToResult(result) {
+            return {
+                playground: playgrounds.find(playground => playground.id === result.ref),
+                ...result
+            }
+        }
+
+        // TODO: Consider adding search metadata to result. Allows text highlighting.
+        return idx.search(value + "*").map(addPlaygroundToResult);
+    };
+
+    // When suggestion is clicked, Autosuggest needs to populate the input
+    // based on the clicked suggestion. Teach Autosuggest how to calculate the
+    // input value for every given suggestion.
+    getSuggestionValue = suggestion => suggestion.playground.name;
+
+    // Use your imagination to render suggestions.
+    renderSuggestion = suggestion => (
+        <div>
+            {suggestion.playground.ref} - {suggestion.playground.name}
+        </div>
+    );
+
+    onChange = (event, {newValue}) => {
+        this.setState({
+            value: newValue
+        });
+        console.log('Autosuggest changed to: ', newValue);
+    };
+
+    // Autosuggest will call this function every time you need to update suggestions.
+    // You already implemented this logic above, so just use it.
+    onSuggestionsFetchRequested = ({value}) => {
+        this.setState({
+            suggestions: this.getSuggestions(value)
+        });
+    };
+
+    // Autosuggest will call this function every time you need to clear suggestions.
+    onSuggestionsClearRequested = () => {
+        this.setState({
+            suggestions: []
+        });
+    };
+
+    render() {
+        const {t, classes, playgrounds, onPlaygroundChange} = this.props;
+        const {value, suggestions} = this.state;
+        if (!playgrounds) return null;
+
+        // Autosuggest will pass through all these props to the input.
+        const inputProps = {
+            placeholder: t('onboarding.playground.search.title'),
+            value,
+            onChange: this.onChange
+        };
+
+        let onSuggestionSelected = (evt, {suggestion}) => {
+            console.log('Selected ', suggestion);
+            onPlaygroundChange(suggestion.playground);
+        };
+
+        // Finally, render it!
         return (
+            <div>
                 <Header
                     // brand="Find your playground"
                     color="rose"
@@ -85,12 +161,25 @@ class SearchBar extends React.Component {
                             <Button justIcon round color="white">
                                 <Search className={classes.searchIcon} />
                             </Button>
+                            <Autosuggest
+                                className={classes.searchIcon}
+                                suggestions={suggestions}
+                                onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                                onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                                getSuggestionValue={this.getSuggestionValue}
+                                onSuggestionSelected={onSuggestionSelected}
+                                renderSuggestion={this.renderSuggestion}
+                                highlightFirstSuggestion={true}
+                                inputProps={inputProps}
+                            />
                         </div>
                     }
                 />
-        )
+            </div>
+        );
     }
 }
+
 const TranslatedSearch = withNamespaces("translations")(SearchBar);
 const PlaygroundSearch = withPlaygrounds(TranslatedSearch);
 export default withStyles(navbarsStyle)(PlaygroundSearch);
