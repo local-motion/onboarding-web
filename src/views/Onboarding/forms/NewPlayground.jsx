d44457f13c1c6942544da45867947ddeb051d7d4
gerard-payne <gerard.payne@capgemini.com>
2018-11-27 15:51:13 +0100

    added material elements

--- src/views/Onboarding/forms/NewPlayground.jsx
+++ src/views/Onboarding/forms/NewPlayground.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { withNamespaces } from "react-i18next";
 import componentsStyle from "assets/jss/material-kit-react/views/components.jsx";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 /*import { graphql } from "react-apollo";
 import gql from "graphql-tag";
 
@@ -41,17 +43,22 @@ class NewPlayground extends React.Component {
         const {classes} = this.props;
 
         return (
-            <form className={classes.container}>
-                <input type="text" placeholder="Playground name" defaultValue={playground.name}/>
-                <input type="text" placeholder="Address" defaultValue={playground.address}/>
-                <input type="text" placeholder="Contact name" defaultValue={playground.contactName}/>
-                <input type="text" placeholder="Contact email" defaultValue={playground.email}/>
+            <div>
+                <h2>Add a playground</h2>
+                <form className={classes.container}>
+                    <TextField className={classes.textField} label="Playground name" defaultValue={playground.name}/>
+                    <TextField className={classes.textField} label="Address" defaultValue={playground.address}/>
+                    <TextField className={classes.textField} label="Contact name" defaultValue={playground.contactName}/>
+                    <TextField className={classes.textField} label="Contact email" defaultValue={playground.contactemail}/>
 
-                <input type="hidden" id={"latitude"} defaultValue={playground.latitude}/>
-                <input type="hidden" id={"longitude"} defaultValue={playground.latitude}/>
-            </form>
-    );
-  }
+                    <input type="hidden" id={"latitude"} defaultValue={playground.latitude}/>
+                    <input type="hidden" id={"longitude"} defaultValue={playground.latitude}/>
+                </form>
+
+                <Button>Save</Button>
+            </div>
+        );
+    }
 }
 
 export default withStyles(componentsStyle)(
