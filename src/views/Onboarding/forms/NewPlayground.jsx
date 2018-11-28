e71e747e65ccc5e55ffed4d1fdc720ad42c1bfaa
gerard-payne <gerard.payne@capgemini.com>
2018-11-28 14:56:55 +0100

    added create initiative

--- src/views/Onboarding/forms/NewPlayground.jsx
+++ src/views/Onboarding/forms/NewPlayground.jsx
@@ -4,52 +4,53 @@ import { withNamespaces } from "react-i18next";
 import componentsStyle from "assets/jss/material-kit-react/views/components.jsx";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import { graphql } from "react-apollo";
 import gql from "graphql-tag";
 import { Mutation } from "react-apollo";
 
 const CREATE_INITIATIVE = gql`
   mutation CreateInitiative($input: CreateInitiativeInput!) {
-    CreateInitiative(input: $input) {
+    createInitiative(input: $input) {
       id
     }
   }
 `;
 
 
-
 class NewPlayground extends React.Component {
-    render() {
-        const {t, classes} = this.props;
-
-        const newInitiative = {
+    constructor(props) {
+        super(props);
+        this.state = {
             name: "",
-            address: "",
-            latitude: "",
-            longitude: "",
-            contactName: "",
-            contactEmail: "",
-            initiativeId: null,
+            lat: this.props.playground.latLng.lat(),
+            lng: this.props.playground.latLng.lng(),
+            initiativeId: ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+                (c ^ crypto.getRandomValues(new Uint8Array(1))[0] && 15 >> c / 4).toString(16)
+            ),
             type: "smokefree",
             status: "not_started"
         };
+    }
+
+    updateName = (eEvent) => {
+        this.setState({
+            name: eEvent.target.value
+        });
+    }
+
+    render() {
+        const {t, classes} = this.props;
 
         return (
             <div>
                 <h2>Add a playground</h2>
-                <form className={classes.container}>
-                    <TextField className={classes.textField} label="Playground name" pattern="/^\w{6,}$/" defaultValue={newInitiative.name}/>
-                    <TextField className={classes.textField} label="Address" pattern="^\w{6,}$" defaultValue={newInitiative.address}/>
-                    <TextField className={classes.textField} label="Contact name" pattern="^\w{6,}$" defaultValue={newInitiative.contactName}/>
-                    <TextField className={classes.textField} label="Contact email" pattern='/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/' defaultValue={newInitiative.contactEmail}/>
 
-                    <input type="hidden" id={"latitude"} defaultValue={newInitiative.latitude}/>
-                    <input type="hidden" id={"longitude"} defaultValue={newInitiative.latitude}/>
+                <form className={classes.container}>
+                    <TextField className={classes.textField} label="Playground name" pattern="/^\w{4,}$/" onKeyUp={this.updateName} defaultValue={this.state.name}/>
                 </form>
 
                 <Mutation mutation={CREATE_INITIATIVE}>
                     {(joinInitiative) => (
-                        <Button onClick={() => joinInitiative({ variables: { input: newInitiative } })}>
+                        <Button onClick={() => joinInitiative({ variables: { input: this.state } })}>
                             {t("onboarding.playground.calltoaction.button")}
                         </Button>
                     )}
