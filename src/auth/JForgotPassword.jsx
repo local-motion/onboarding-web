dbfd2f51194d53b7164ff8ff9b199e9c477c43b5
Errrrk <43416374+errrrk@users.noreply.github.com>
2018-11-28 07:39:44 +0100

    Custom signup

---
+++ src/auth/JForgotPassword.jsx
@@ -0,0 +1,109 @@
+import React, {Component} from 'react';
+import {Button, Input} from '@material-ui/core'
+import {Auth, Logger} from 'aws-amplify';
+
+const logger = new Logger('JForgotPassword');
+
+/**
+ * A mix between
+ * https://raw.githubusercontent.com/aws-amplify/amplify-js/master/packages/aws-amplify-react/src/Auth/ForgotPassword.jsx
+ * https://github.com/richardzcode/Journal-AWS-Amplify-Tutorial/blob/master/step-03/journal/src/components/auth/JforgotPassword.jsx
+ */
+export default class JForgotPassword extends Component {
+    constructor(props) {
+        super(props);
+        this.sendCode = this.sendCode.bind(this);
+        this.changeState = this.changeState.bind(this);
+        this.inputs = {};
+        this.state = {error: ''}
+    }
+
+    changeState(state, data) {
+        const {onStateChange} = this.props;
+        if (onStateChange) {
+            onStateChange(state, data);
+        }
+    }
+
+    sendCode() {
+        const username = this.props.authData || this.inputs.username;
+        logger.info('resend code to ' + username);
+        Auth.forgotPassword(username)
+            .then(data => this.sendSuccess(username, data))
+            .catch(err => this.handleError(err));
+    }
+
+    sendSuccess(username, data) {
+        logger.info('sent code for ' + username, data);
+        this.changeState('forgotPasswordReset', username);
+    }
+
+    handleError(err) {
+        logger.info('forgot password send code error', err);
+        this.setState({error: err.message || err});
+    }
+
+    render() {
+        const {authState, authData} = this.props;
+        if (authState !== 'forgotPassword') {
+            return null;
+        }
+
+        const style = {
+            width: '20rem',
+            links: {fontSize: '0.9em'},
+            button: {width: '100%'},
+            alert: {fontSize: '0.8em'}
+        };
+
+        const {error} = this.state;
+
+        return (
+            <div style={style.container}>
+                <div>
+                    <h1>Forgot password</h1>
+                </div>
+                <form>
+                    <div>
+                        <Input
+                            type="text"
+                            placeholder="Username"
+                            defaultValue={authData || ''}
+                            onChange={event => this.inputs.username = event.target.value}
+                            autoFocus
+                        />
+                    </div>
+                    <div>
+                        <Button style={style.button} onClick={this.sendCode}>Send password reset code</Button>
+                    </div>
+                    {error && <div style={style.alert}>{error}</div>}
+                </form>
+                <div style={style.links}>
+                    <div>
+                        <a
+                            style={style.button}
+                            onClick={() => this.changeState('signIn')}>
+                            Back to sign in
+                        </a>
+                    </div>
+                    <div>
+                        <p> No account? </p>
+                        <Button
+                            style={style.button}
+                            onClick={() => this.changeState('confirmSignUp')}>
+                            Confirm a code
+                        </Button>
+                    </div>
+                    <div>
+                        <p> No account? </p>
+                        <Button
+                            style={style.button}
+                            onClick={this.signUp}>
+                            Create account
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {createBrowserHistory} from "history";
-import {Router, Route, Switch} from "react-router-dom";
+import {Route, Router, Switch} from "react-router-dom";
 import {I18nextProvider} from "react-i18next";
 import i18n from "./i18n";
 import {ApolloClient} from 'apollo-client';
 import {HttpLink} from 'apollo-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {ApolloProvider} from 'react-apollo';
-import { setContext } from "apollo-link-context";
+import {setContext} from "apollo-link-context";
 
 import indexRoutes from "routes/index.jsx";
 import "assets/scss/material-kit-react.css?v=1.2.0";
 
 
 import Amplify from "aws-amplify";
-import {Auth} from 'aws-amplify';
-// import { Hub, Logger } from 'aws-amplify';
-import { withAuthenticator } from 'aws-amplify-react';
+import {Greetings, withAuthenticator} from 'aws-amplify-react';
 
+import JSignOut from "auth/JSignOut";
+import JSignUp from "auth/JSignUp";
+import JSignIn from "auth/JSignIn";
+import JConfirmSignUp from "auth/JConfirmSignUp";
+import JConfirmSignIn from "auth/JConfirmSignUp";
+import JForgotPassword from "./auth/JForgotPassword";
+import JForgotPasswordReset from "./auth/JForgotPasswordReset";
+
+const isAuthenticated = () => Amplify.Auth.user != null;
 const oauth = {
     awsCognito: {
         // Domain name
@@ -80,59 +87,23 @@ Amplify.configure({
     // ...
 });
 
-/*
-const alex = new Logger('Alexander_the_auth_watcher');
-
-alex.onHubCapsule = (capsule) => {
-    // eslint-disable-next-line
-    switch (capsule.payload.event) {
-
-        case 'signIn':
-            alex.error('user signed in'); //[ERROR] Alexander_the_auth_watcher - user signed in
-            break;
-        case 'signUp':
-            alex.error('user signed up');
-            break;
-        case 'signOut':
-            alex.error('user signed out');
-            break;
-        case 'signIn_failure':
-            alex.error('user sign in failed');
-            break;
-        case 'configured':
-            alex.error('the Auth module is configured');
-
-    }
-}
-
-Hub.listen('auth', alex);
-*/
-
-const config = Auth.configure();
-const {
-    domain,
-    redirectSignIn,
-    // eslint-disable-next-line
-    redirectSignOut,
-    responseType
-} = config.oauth.awsCognito;
-const clientId = config.userPoolWebClientId;
-// The url of the Cognito Hosted UI
-// eslint-disable-next-line
-const url = 'https://' + domain + '/login?redirect_uri=' + redirectSignIn + '&response_type=' + responseType + '&client_id=' + clientId;
-// Launch hosted UI
-// debugger;
-// window.location.assign(url);
-
 let hist = createBrowserHistory();
 const rootEl = document.querySelector("#root");
 
-class MyApp extends React.Component {
+const AlwaysOn = (props) => {
+    return (
+        <div>
+            <div>I am always here to show current auth state: {props.authState}</div>
+            <button onClick={() => props.onStateChange('signIn')}>Show Sign In</button>
+        </div>
+    )
+};
 
+const App = class App extends React.Component {
     render() {
         const {auth, authData} = this.props;
-        console.log(auth);
-        console.log(authData);
+        console.log('Auth: ', auth);
+        console.log('Auth data: ', authData);
         if (!authData) {
             return "Logging in failed: " + (auth && auth.state);
         }
@@ -141,8 +112,8 @@ class MyApp extends React.Component {
         console.log('ONBOARDING_API: ' + process.env.ONBOARDING_API);
         console.log('Using Onboarding API at ' + uri);
 
-        const httpLink = new HttpLink({ uri: uri });
-        const authLink = setContext(async (req, { headers }) => {
+        const httpLink = new HttpLink({uri: uri});
+        const authLink = setContext(async (req, {headers}) => {
             let session = authData.getSignInUserSession();
             let idToken = session.getIdToken();
             let jwtToken = idToken.getJwtToken();
@@ -155,21 +126,22 @@ class MyApp extends React.Component {
         });
         const link = authLink.concat(httpLink);
         const client = new ApolloClient({
-            // By default, this client will send queries to the
-            //  `/graphql` endpoint on the same host
-            // Pass the configuration option { uri: YOUR_GRAPHQL_API_URL } to the `HttpLink` to connect
-            // to a different host
             link: link,
             cache: new InMemoryCache(),
         });
 
         return (
             <div>
-{/*
-                <button onClick={this.props.OAuthSignIn}>
-                    Sign in with AWS
-                </button>
-*/}
+                {
+                    <div>Hello, {authData.username}</div>
+                }
+                <JSignOut/>
+                {
+                    !isAuthenticated() &&
+                    <button onClick={this.props.OAuthSignIn}>
+                        Sign in with AWS
+                    </button>
+                }
                 <ApolloProvider client={client}>
                     <I18nextProvider i18n={i18n}>
                         <Router history={hist}>
@@ -184,11 +156,19 @@ class MyApp extends React.Component {
             </div>
         )
     }
-}
+};
 
-const Bla = withAuthenticator(MyApp);
+const SecuredApp = withAuthenticator(App, false, [
+    <JSignIn />,
+    <JSignUp />,
+    <JForgotPassword />,
+    <JForgotPasswordReset />,
+    <JConfirmSignIn />,
+    <JConfirmSignUp />,
+    <AlwaysOn/>
+]);
 
 ReactDOM.render(
-    <Bla/>,
+    <SecuredApp/>,
     rootEl
 );
