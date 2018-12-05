a2e623352e10410ad6c0117f9c3c100460593d85
Anand Aili <anandaili08@gmail.com>
2018-12-05 09:15:09 +0100

    Update techoverflow-d env Cognito details

--- src/index.js
+++ src/index.js
@@ -29,16 +29,16 @@ const isAuthenticated = () => Amplify.Auth.user != null;
 const oauth = {
     awsCognito: {
         // Domain name
-        domain: 'smokefree-dev.auth.eu-west-1.amazoncognito.com',
+        domain: 'techoverflow-d.auth.eu-west-1.amazoncognito.com',
 
         // Authorized scopes
         scope: ['email', 'openid'],
 
         // Callback URL
-        redirectSignIn: 'http://localhost:3000/onboarding/signin',
+        redirectSignIn: 'https://techoverflow-d.aws.nl.eu.abnamro.com/onboarding/signin',
 
         // Sign out URL
-        redirectSignOut: 'http://localhost:3000/onboarding/logout',
+        redirectSignOut: 'https://techoverflow-d.aws.nl.eu.abnamro.com/onboarding/logout',
 
         // 'code' for Authorization code grant,
         // 'token' for Implicit grant
@@ -57,9 +57,9 @@ Amplify.configure({
         // REQUIRED - Amazon Cognito Region
         region: 'eu-west-1',
         // OPTIONAL - Amazon Cognito User Pool ID
-        userPoolId: 'eu-west-1_WsTxYUHyC',
+        userPoolId: 'eu-west-1_qWMNA59fh',
         // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-        userPoolWebClientId: '3nkh1qomocr39s893jf0dp44cd',
+        userPoolWebClientId: '511ukdonr6co8ial59gj5hf4t7',
 
         // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
         mandatorySignIn: false,
