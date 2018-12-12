8be2664f57bc1c419f4f792e563bb77aeff85aad
dylandejonge <dylan_dejonge@hotmail.com>
2018-12-12 14:06:36 +0100

    Error Dialog

--- src/views/Workspace/forms/SmokefreeDate.jsx
+++ src/views/Workspace/forms/SmokefreeDate.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
-import {withNamespaces} from "react-i18next";
+import { withNamespaces } from "react-i18next";
 import componentsStyle from "assets/jss/material-kit-react/views/components.jsx";
 // @material-ui/icons
-import Dialog from '@material-ui/core/Dialog';
+import CustomDialog from 'components/Dialogs/CustomDialog.jsx';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import {Mutation} from "react-apollo";
+import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
 // core components
 
@@ -23,7 +23,7 @@ class SmokefreeDate extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            startDate: props.startDate || new Date()
+            startDate: props.startDate || new Date(),
         };
     }
 
@@ -42,11 +42,24 @@ class SmokefreeDate extends React.Component {
     };
 
     _onError = error => {
-        console.log("Could not change smoke-free date", error);
+        const errorString = error.toString();
+        let errorTranslation = "";
+        switch (errorString){
+            case "TypeError: Cannot read property 'data' of undefined":
+                errorTranslation = "Je hebt onvoldoende rechten om deze actie uit te voeren";
+                break;
+            default:
+                errorTranslation = "Er is een fout opgetreden, probeer het later nog eens";
+
+        }
+
+        this.setState({errorMessage: errorTranslation});
     };
 
     render() {
         const {classes} = this.props;
+        const { errorMessage } = this.state;
+        console.log(errorMessage);
         return (
             <Mutation mutation={SET_SMOKEFREE_DATE} update={null} onError={this._onError}>
                 {(setSmokeFreeDate, { loading, error }) => (
@@ -55,19 +68,19 @@ class SmokefreeDate extends React.Component {
                             dateFormat="YYYY-MM-dd"
                             selected={this.state.startDate}
                             onChange={(date) => {
-                                setSmokeFreeDate({
-                                    variables: {
+                                setSmokeFreeDate({ variables: {
                                         input: {
                                             smokeFreeDate: SmokefreeDate._dateToString(date),
                                             initiativeId: window.location.pathname.split("/").pop()
                                         }
-                                    }
-                                });
+                                    }});
                                 this._changeState(date);
                             }}
                         />
                         {loading && <p>Loading...</p>}
-                        {error && <Dialog open={true} className={classes.container}>{error.toString()}</Dialog>}
+                        {error &&
+                        <CustomDialog title={"Er is een fout opgetreden"} content={errorMessage}></CustomDialog>
+                        }
                     </div>
                 )}
             </Mutation>
