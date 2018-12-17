95421b087436558cf1384e4a5cc021cf6fa91035
dylandejonge <dylan_dejonge@hotmail.com>
2018-12-17 10:14:52 +0100

    Legal pages

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,19 @@
 import Workspace from "views/Workspace/Workspace.jsx";
 import Onboarding from "views/Onboarding/Onboarding.jsx";
+import About from "views/about/About.jsx";
+import Contact from "views/about/Contact.jsx";
+import Terms from "views/legal/Terms.jsx";
+import Privacy from "views/legal/Privacy.jsx";
 
 export default [
+
+    {path: "/about", name: "Who are we", component: About},
+    {path: "/contact", name: "Contact us", component: Contact},
+    {path: "/privacy", name: "Privacy Statement", component: Privacy},
+    {path: "/terms", name: "Terms of Use", component: Terms},
+
     {path: "/workspace", name: "Workspace", component: Workspace},
-    {path: "/", name: "Onboarding", component: Onboarding}
+
+    {path: "/", name: "Onboarding", component: Onboarding},
+
 ];
