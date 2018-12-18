d4abc0223206c19cd560613d2683fdbaec93c94b
dylandejonge <dylan_dejonge@hotmail.com>
2018-12-18 17:29:57 +0100

    Adds (conditional) styling to the team list. Adds avatars with the first letter of the manager name Map through playground managers and hide if no manager are there Edited the volunteer text

--- src/views/Workspace/Sections/PlaygroundManagers.jsx
+++ src/views/Workspace/Sections/PlaygroundManagers.jsx
@@ -4,26 +4,39 @@ import withStyles from "@material-ui/core/styles/withStyles";
 // @material-ui/icons
 // core components
 import pillsStyle from "assets/jss/material-kit-react/views/componentsSections/pillsStyle.jsx";
-import { withNamespaces } from "react-i18next";
+import {withNamespaces} from "react-i18next";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+
 
 class PlaygroundManagers extends React.Component {
     render() {
-        const { playground, profile } = this.props;
-        if(!playground || !profile) return "Loading...";
+        const {playground, profile} = this.props;
+        if (!playground || !profile) return "Loading...";
         console.info(`Displaying ${playground.managers ? playground.managers.length : 0} managers for playground`, playground);
         return (
             <div>
-                <div>
-                    {playground.volunteerCount} vrijwilligers helpen met rookvrij maken.
+                <p>
+                    Er zijn in totaal {playground.volunteerCount} vrijwilligers die helpen met rookvrij maken van deze speeltuin.
+                </p>
+                <div class="manager-container" className={playground.managers.length > 0 ? '' : 'hide'}>
+                    <h3 style={{fontSize: "18px"}}>
+                        Beheerders van de speeltuin :
+                    </h3>
+                    <List>
+                        {playground.managers.map(function (manager, index) {
+                            return <ListItem key={index}>
+                                <ListItemAvatar>
+                                    <Avatar>{manager.username.charAt(0)}</Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary={manager.username} secondary={manager.id === profile.id ? '(You)' : ''}/>
+                            </ListItem>
+                        })}
+                    </List>
                 </div>
-                <div>
-                    Beheerders van deze speeltuin zijn:
-                </div>
-                <ul>
-                    {playground.managers.map(function(manager, index){
-                        return <li key={ index }>{manager.username} <b>{manager.id === profile.id ? '(You)' : ''}</b></li>;
-                    })}
-                </ul>
             </div>
         );
     }
