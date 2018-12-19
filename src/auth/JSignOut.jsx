7d1ee763af81c7105a6e2572fa6097a67add392d
dylandejonge <dylan_dejonge@hotmail.com>
2018-12-19 11:45:04 +0100

    Adds proper responsive menu to application

--- src/auth/JSignOut.jsx
+++ src/auth/JSignOut.jsx
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
-import {Button } from '@material-ui/core'
-import { Auth, Logger } from 'aws-amplify';
+import React, {Component} from 'react';
+import {Button} from '@material-ui/core'
+import {Auth, Logger} from 'aws-amplify';
 
 const logger = new Logger('JSignOut');
 
 export default class JSignOut extends Component {
-  constructor(props) {
-    super(props);
-    this.signOut = this.signOut.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.signOut = this.signOut.bind(this);
+    }
 
-  signOut() {
-    Auth.signOut()
-      .then(() => logger.info('sign out success'))
-      .catch(err => logger.info('sign out error', err));
-  }
+    signOut() {
+        Auth.signOut()
+            .then(() => logger.info('sign out success'))
+            .catch(err => logger.info('sign out error', err));
+    }
 
-  render() {
-    return (
-      <Button style={{ textDecoration: 'underline' }} onClick={this.signOut}>
-        Sign Out
-      </Button>
-    )
-  }
+    render() {
+        const {layout} = this.props;
+        return (
+            <div>
+                <a href={"#"} style={{textDecoration: 'underline', padding: '10px 20px', display: (layout !== 'list' ? 'none' : 'inline-block') }} onClick={this.signOut}>
+                    Uitloggen
+                </a>
+                <Button style={{textDecoration: 'underline', display: (layout === 'list' ? 'none' : 'inline-block') }} onClick={this.signOut}>
+                    Sign Out
+                </Button>
+            </div>
+        )
+    }
 }
