3d808ed8abb4aaf247bcad6364a510c883432bac
Dimitri Dekker <dimitri.dekker@nl.abnamro.com>
2019-01-18 10:35:19 +0100

    working on mutation

--- src/components/Playground/PlaygroundReducer.js
+++ src/components/Playground/PlaygroundReducer.js
@@ -1,10 +1,12 @@
-import { GET_PLAYGROUNDS } from "./PlaygroundActions";
+import { GET_PLAYGROUNDS, CREATE_INITIATIVE } from "./PlaygroundActions";
 import { SUCCESS_POSTFIX } from "../../GlobalActions";
 
 const playgroundReducer = (state = [], action) => {
   switch (action.type) {
     case GET_PLAYGROUNDS + SUCCESS_POSTFIX:
       return action.payload.data.playgrounds
+    // case CREATE_INITIATIVE + SUCCESS_POSTFIX:
+    //   return [action.payload.data, ...state]  TO BE CHECKED HOW RETURN DATA WILL LOOK LIKE
     default:
       return state
   }
