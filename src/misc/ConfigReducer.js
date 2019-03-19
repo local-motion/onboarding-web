842681bea529099fb2cdfa840b09294578e70652
Dimitri Dekker <dimitri_dekker@hotmail.com>
2019-03-19 17:50:31 +0100

    Config from server (#339)
    * moved the main app from index to separate file
    
    * running with cognito and google config from server

--- src/misc/ConfigReducer.js
+++ src/misc/ConfigReducer.js
@@ -1,17 +1,21 @@
-import { PUBLISH_ENVIRONMENT, PUBLISH_GRAPHQLCLIENT } from "./ConfigActions";
+import { PUBLISH_ENVIRONMENT, PUBLISH_GRAPHQLCLIENT, PUBLISH_API_BASE_URL } from "./ConfigActions";
+import { SUCCESS_POSTFIX } from "../api/QueryActions";
 
 // State definition
 
 const initialState = {
   // environmentProperties:     set of various environment properties
+  // apiBaseUrl:                base path (incl scheme) of the LocalMotion api's
   // graphQLClient:             client to be used for doing graphQL calls
 }
 
 
 // Selectors
 
-export const getGraphQLClient= (state) => state.config.graphQLClient
-export const getEnvironmentProperties= (state) => state.config.environmentProperties
+export const getGraphQLClient = (state) => state.config.graphQLClient
+export const getEnvironmentProperties = (state) => state.config.environmentProperties
+export const getApiBaseUrl = (state) => state.config.apiBaseUrl
+export const getGoogleMapsKey = (state) => state.config.environmentProperties.googleMapsKey
 
 
 // Reducer
@@ -19,10 +23,16 @@ export const getEnvironmentProperties= (state) => state.config.environmentProper
 const configReducer = (state = initialState, action) => {
   switch (action.type) {
 
-    case PUBLISH_ENVIRONMENT:
+    case PUBLISH_ENVIRONMENT + SUCCESS_POSTFIX:
       return {
         ...state,
-        environmentProperties: action.environmentProperties
+        environmentProperties: action.payload
+      }
+
+    case PUBLISH_API_BASE_URL:
+      return {
+        ...state,
+        apiBaseUrl: action.url
       }
 
     case PUBLISH_GRAPHQLCLIENT:
---
+++ src/misc/Paths.js
@@ -0,0 +1,8 @@
+/**
+ * This file list the endpoints of the services used in localmotion.
+ * They should be prepended by the api base url
+ */
+
+export const CONFIGURATION_PATH =     'configuration'
+export const CHAT_PATH =              'chatbox'
+export const GRAPHQL_PATH =           'graphql'
