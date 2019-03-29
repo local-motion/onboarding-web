f9e959d5f958241a11a88249872820fb9e549419
Karafizi Arthur <karafizi.artur@gmail.com>
2019-03-29 22:58:27 +0300

    Redesign team chat page

--- src/components/Chatbox/views/ChatBox.js
+++ src/components/Chatbox/views/ChatBox.js
@@ -3,27 +3,56 @@ import ChatMessageList from './ChatMessageList';
 import ChatMessageEntryBox from './ChatMessageEntryBox';
 import { withStyles } from '@material-ui/core/styles';
 import { Paper, Grid } from '@material-ui/core';
+import debounce from 'lodash/debounce';
 
 const styles = theme => ({
+  chatTitleContainer: {
+    padding: '20px 25px',
+    borderBottom: '1px solid #e7e7e7',
+  },
+  chatTitle: {
+    fontSize: '30px',
+    fontFamily: '"dk_black_bamboo-webfont"',
+    margin: '5px 0',
+    textAlign: 'left',
+    color: '#626262',
+    textTransform: 'uppercase',
+  },
   chatBox: {
     display: "flex",
     height: '100%',
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
+    boxShadow: `1px 1px 5px 0px rgba(0,0,0,0.2)`
   },
   list: {
-    margin: '4px 0px 2px 0px',
+    background: '#fcfcfc',
+    margin: '4px 0 0',
     overflow: 'auto',
     scrollbarWidth: 'none',       // hide the scrollbar in firefox
     // WebkitScrollbar: {
     //   display: 'none'
     // }
   },
+  chatWrapper: {
+    borderTop: '1px solid #e7e7e7',
+    boxShadow: '0px 0px 10px 0px rgba(98,98,98,0.1)',
+  },
 })
 
 
 class ChatBox extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.activateTyping = this.activateTyping.bind(this);
+    this.deactivateTyping = debounce(this.deactivateTyping.bind(this), 1000);
+    this.onTextChange = this.onTextChange.bind(this);
+  }
 
   state = {
-    scrolledToBottom: true
+    scrolledToBottom: true,
+    isTyping: false,
   }
 
   onSubmitHandler = () => {
@@ -59,19 +88,37 @@ class ChatBox extends React.Component {
     this.setState({scrolledToBottom})
   }
 
+  deactivateTyping() {
+    this.setState({ isTyping: false });
+  }
+
+  activateTyping() {
+    this.setState({ isTyping: true });
+
+    this.deactivateTyping();
+  }
+
+  onTextChange(text) {
+    this.props.onChangeHandler(text);
+    this.activateTyping();
+  }
+
   render() {
-    const {classes, chatMessages=[], messageText='', onChangeHandler, chatDisabled, userName} = this.props
+    const {classes, chatMessages=[], messageText='', chatDisabled, userName} = this.props
     return (
-      <Paper elevation={1} className={classes.chatBox}>
+      <Paper elevation={2} className={classes.chatBox}>
         <Grid container direction="column" wrap="nowrap">
+          <div className={classes.chatTitleContainer}>
+            <div className={classes.chatTitle}>Chat</div>
+          </div>
           <Grid item xs  id="chatPane" className={classes.list} onScroll={() => this.onMessageListScroll()}>
-            <ChatMessageList items={chatMessages} userName={userName} />
+            <ChatMessageList items={chatMessages} userName={userName} isTyping={this.state.isTyping} />
           </Grid>
-          <Grid item xs="auto">
+          <Grid item xs="auto" className={classes.chatWrapper}>
             {!chatDisabled &&
-              <ChatMessageEntryBox 
+              <ChatMessageEntryBox
                 disabled={chatDisabled}
-                onTextChange={onChangeHandler} 
+                onTextChange={this.onTextChange}
                 onSubmitClick={this.onSubmitHandler} 
                 text={messageText} 
               />
@@ -79,7 +126,7 @@ class ChatBox extends React.Component {
           </Grid>
         </Grid>
       </Paper>
-  )
+    )
   }
 }
 
--- src/components/Chatbox/views/ChatMessageItem.js
+++ src/components/Chatbox/views/ChatMessageItem.js
@@ -2,37 +2,90 @@ import React from 'react'
 import { ListItem, Avatar, Typography, ListItemAvatar, Grid } from '@material-ui/core';
 import withStyles from "@material-ui/core/styles/withStyles";
 import { deepPurple } from "@material-ui/core/colors";
+import EditPencil from "assets/img/edit-pencil.svg";
 
 const styles = theme => ({
     avatar: {
         marginRight: 10,
-      },
+        alignSelf: 'flex-start',
+    },
     purpleAvatar: {
         marginRight: 10,
         color: '#fff',
         backgroundColor: deepPurple[500],
-      },  
+        alignSelf: 'flex-start',
+    },
+    userInfo: {
+        marginBottom: '5px',
+    },
     authorText: {
         align: 'left',
-        color: 'black',
-        fontWeight: 'bold',
         gutterBottom: 'true',
-        marginRight: '10px',
+        marginRight: '5px',
         noWrap: 'true',
+        color: '#085ca6',
+        fontSize: '13px',
+        fontWeight: 700,
     },
     dateText: {
         align: 'left',
-        color: '#555',
         gutterBottom: 'true',
         marginRight: '10px',
         noWrap: 'true',
+        color: '#6f747e',
+        fontSize: '13px',
+        fontWeight: 500,
+        margin: '0 30px',
+        position: 'relative',
+        '&:before': {
+            content: '"●"',
+            position: 'absolute',
+            left: '-20px',
+        },
     },
     contentText: {
         align: 'left',
-        color: 'black',
-        fontSize: '100%'
+        color: '#fff',
+        fontSize: '13px',
+        lineHeight: 1.3,
+        position: 'relative',
+        padding: '7px 10px',
+        background: '#085ca6',
+        fontWeight: 600,
+        borderRadius: '15px',
+        borderTopLeftRadius: '3px',
+        width: 'fit-content',
     },
-})
+    typingDot: {
+        display: 'inline-block',
+        width: '3px',
+        height: '3px',
+        borderRadius: '50%',
+        marginRight: '3px',
+        background: '#6f747e',
+        animation: 'wave 1.3s linear infinite',
+    },
+    typingDotSecond: {
+        animationDelay: '-1.1s',
+    },
+    typingDotThird: {
+        animationDelay: '-0.9s',
+    },
+    typingText: {
+        marginLeft: '5px',
+        marginRight: '5px',
+        fontSize: '12px',
+        fontWeight: 'bold',
+        color: '#626262',
+    },
+    typingIcon: {
+        display: 'inline-block',
+        background: `url('${EditPencil}') no-repeat`,
+        width: '16px',
+        height: '14px',
+        verticalAlign: 'middle',
+    }
+});
 
 const isSameDate = (date1, date2) => date1.getFullYear() === date2.getFullYear() && date1.getMonth() === date2.getMonth() && date1.getDate() === date2.getDate()
 const getTimeString = date => date.getHours() + ':' + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes()
@@ -57,25 +110,42 @@ const getPrettyMessageDatetime = messageDateString => {
         return messageDate.toLocaleDateString('nl-NL', displayOptions) + ' ' + getTimeString(messageDate)
 }
 
-const ChatMessageItem = ({userName, author, creationTime, text, classes}) => {
+
+
+const ChatMessageItem = ({userName, author, creationTime, text, classes, isTyping}) => {
     const iconText = (author || '??').substring(0, 2)
+
+    const TypingText = () => (
+      <React.Fragment>
+          <span className={classes.typingIcon} />
+          <span className={classes.typingText}>Typing</span>
+          <span className={classes.typingDot} />
+          <span className={`${classes.typingDot} ${classes.typingDotSecond}`} />
+          <span className={`${classes.typingDot} ${classes.typingDotThird}`} />
+      </React.Fragment>
+    );
 
     return (
         <ListItem>
             <ListItemAvatar>
-                <Avatar className={author === userName ? classes.purpleAvatar : classes.avatar }>
+                <Avatar className={author === userName ? classes.purpleAvatar : classes.avatar}>
                     {iconText}
                 </Avatar>
             </ListItemAvatar>
             <Grid container direction="column">
                 <Grid item>
-                    <Grid container direction="row">
-                    <Grid item><Typography className={classes.authorText}>{author}</Typography></Grid>
-                    <Grid item><Typography className={classes.dateText}>{getPrettyMessageDatetime(creationTime)}</Typography></Grid>
-                </Grid>
+                    <Grid container direction="row" className={classes.userInfo}>
+                        <Grid item><Typography className={classes.authorText}>{author}</Typography></Grid>
+                        <Grid item><Typography
+                          className={classes.dateText}>{getPrettyMessageDatetime(creationTime)}</Typography></Grid>
+                    </Grid>
                 </Grid>
                 <Grid item>
-                    <Typography className={classes.contentText}>{text}</Typography>
+                    {
+                        isTyping
+                          ? <TypingText />
+                          : <Typography className={classes.contentText}>{text}</Typography>
+                    }
                 </Grid>
             </Grid>
         </ListItem>
