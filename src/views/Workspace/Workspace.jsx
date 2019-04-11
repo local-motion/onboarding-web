d85fa9db19611b234c5bd8bd44b0e879a235fd20
Karafizi Arthur <karafizi.artur@gmail.com>
2019-04-11 11:05:16 +0300

    Fix error after logging in

--- src/views/Workspace/Workspace.jsx
+++ src/views/Workspace/Workspace.jsx
@@ -70,7 +70,7 @@ class WorkspaceTemplate extends React.Component {
     }
 
     render() {
-        const { playground, user, classes, ...rest } = this.props;
+        const { playground, user, classes, signInHandler, ...rest } = this.props;
 
         // const activePhaseIdx = this.getStatusIndex();                        // the active phase represents the current state of this playground
         // const phaseIdx = this.props.match.params.phaseId - 1;                // The phase that the user has selected, if any
@@ -95,6 +95,7 @@ class WorkspaceTemplate extends React.Component {
                     playground={playground}
                     phases={phases}
                     activePhase={activePhase}
+                    signInHandler={signInHandler}
                     user={user}
                     classes={classes}
                     rest={rest}
