0944b8e71eb60caf807128da6678210df7e1ac88
Karafizi Arthur <karafizi.artur@gmail.com>
2019-05-21 15:16:27 +0300

    Change logo to textual one named Rookvrij spelen

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ import Drawer from "@material-ui/core/Drawer";
 // @material-ui/icons
 import Menu from "@material-ui/icons/Menu";
 // core components
-import headerStyle from "assets/jss/material-kit-react/components/headerStyle.jsx";
+import headerStyle from "../../assets/jss/material-kit-react/components/headerStyle.jsx";
 import { connect } from 'react-redux'
 import { getUser } from "../UserProfile/UserProfileReducer";
 import { Button } from "@material-ui/core";
@@ -79,7 +79,6 @@ class Header extends React.Component {
             color,
             rightLinks,
             leftLinks,
-            brand,
             fixed,
             absolute,
             user,
@@ -90,24 +89,14 @@ class Header extends React.Component {
             [classes[color]]: color,
             [classes.absolute]: absolute,
             [classes.fixed]: fixed
-        })
-        const brandContent = <img src={require("assets/img/logo-horizontal.png")} alt={"Rookvrije generatie logo"} style={{width: "250px"}} />;
-        const brandComponent = brand ? <div align='center'><Link to={'/'}>{brandContent}</Link></div> : null;
+        });
 
         return (
             <AppBar className={appBarClasses + " lm-header"}>
                 <Toolbar className={classes.container}>
-
-                    {leftLinks !== undefined ? brandComponent : null}
-                    <div className={classes.flex}>
-                        {leftLinks !== undefined ? (
-                            <Hidden smDown implementation="css">
-                                {leftLinks}
-                            </Hidden>
-                        ) : (
-                            brandComponent
-                        )}
-                    </div>
+                    <Link to={'/'}>
+                        <div className={classes.logo}>Rookvrij<span>spelen</span></div>
+                    </Link>
 
                     <Hidden smDown implementation="css">
                         {rightLinks}
--- src/views/Onboarding/Sections/OnboardingHeader.jsx
+++ src/views/Onboarding/Sections/OnboardingHeader.jsx
@@ -31,11 +31,22 @@ const styles = theme => ({
     logo: {
         width: 300,
         padding: 25,
+        fontSize: 30,
+        color: 'rgb(8, 92, 166)',
+        fontFamily: 'dk_black_bamboo-webfont',
+        letterSpacing: '1px',
+        lineHeight: 1.2,
 
         [theme.breakpoints.down('sm')]: {
             width: 200,
             margin: 20,
             padding: 0,
+        },
+
+        '& > span': {
+            color: 'rgb(207, 26, 49)',
+            letterSpacing: '0px',
+            marginLeft: 5,
         },
     },
     menu: {
@@ -99,11 +110,7 @@ const OnboardingHeader = ({ classes, history, location, user }) => {
     return (
       <AppBar className={classes.landingAppBar}>
           <Link to={'/'}>
-              <img
-                src={require("../../../assets/img/landing/logo.png")}
-                alt={"Rookvrije generatie logo"}
-                className={classes.logo}
-              />
+              <div className={classes.logo}>Rookvrij<span>spelen</span></div>
           </Link>
 
           <HeaderLinks className={classes.menu} />
